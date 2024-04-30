cf8a036d891436edb24f2c70c62fd8b97d6fe9fd
Kazuki Takahashi <narita30830613@gmail.com>
2024-04-30 21:37:05 +0900

    figma to code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,101 @@
+import type { NextPage } from "next";
+import styles from "./index.module.css";
+// import Head from "next/head";
 import Image from "next/image";
+import Head_tag from "@/components/Head";
 
-export default function Home() {
+const ProfilePC: NextPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <div className={styles.profilePc}>
+      {/* <img className={styles.logoIcon} alt="" src="logo.png" /> */}
+      <Image
+        className={styles.logoIcon}
+        src="/logo.png"
+        alt="logo"
+        width={120}
+        height={40}
+      />
+      <div className={styles.about}>About</div>
+      <div className={styles.bicycle}>Bicycle</div>
+      <div className={styles.about1}>About</div>
+      {/* <img className={styles.icon} alt="" src="線 1.svg" /> */}
+      <Image
+        className={styles.icon}
+        src="/線 1.svg"
+        alt="icon"
+        width={106}
+        height={0}
+      />
+      <div className={styles.kakeruMiyaichi}>KAKERU MIYAICHI</div>
+      <div className={styles.div}>
+        <p className={styles.p}>
+          テキストテキストテキストテキストテキストテキストテキスト
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <p className={styles.p}>
+          テキストテキストテキストテキストテキストテキストテキスト
+        </p>
+        <p className={styles.p}>
+          テキストテキストテキストテキストテキストテキストテキスト
+        </p>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <div className={styles.bicycle1}>Bicycle</div>
+      {/* <img className={styles.icon1} alt="" src="線 2.svg" /> */}
+      <Image
+        className={styles.icon1}
+        src="/線 2.svg"
+        alt="icon"
+        width={122}
+        height={0}
+      />
+      <div className={styles.div1}>タイトルタイトル</div>
+      <div className={styles.div2}>テキストテキストテキスト</div>
+      <div className={styles.div3}>タイトルタイトル</div>
+      <div className={styles.div4}>テキストテキストテキスト</div>
+      <div className={styles.div5}>タイトルタイトル</div>
+      <div className={styles.div6}>テキストテキストテキスト</div>
+      <div className={styles.profile}>© 2020 Profile</div>
+      {/* <img className={styles.maskGroupIcon} alt="" src="Mask group2.png" />
+      <img className={styles.maskGroupIcon1} alt="" src="Mask group1.png" />
+      <img className={styles.bicycle1Icon} alt="" src="bicycle1.png" />
+      <img className={styles.bicycle2Icon} alt="" src="bicycle2.png" />
+      <img className={styles.bicycle3Icon} alt="" src="bicycle3.png" /> */}
+      <Image
+        className={styles.maskGroupIcon}
+        src="/main_visual.png"
+        alt="main_visual"
+        width={1280}
+        height={600}
+      />
+      <Image
+        className={styles.maskGroupIcon1}
+        src="/about.png"
+        alt="about"
+        width={100}
+        height={100}
+      />
+      <Image
+        className={styles.bicycle1Icon}
+        src="/bicycle1.png"
+        alt="bike1"
+        width={274.4}
+        height={182}
+      />
+      <Image
+        className={styles.bicycle2Icon}
+        src="/bicycle2.png"
+        alt="bike2"
+        width={274.4}
+        height={182}
+      />
+      <Image
+        className={styles.bicycle3Icon}
+        src="/bicycle3.png"
+        alt="bike3"
+        width={274.4}
+        height={182}
+      />
+    </div>
   );
-}
+};
+
+export default ProfilePC;
