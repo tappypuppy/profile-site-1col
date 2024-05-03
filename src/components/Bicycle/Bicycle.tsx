3104d1828cbbef88f92b584b87b90eafe9fa60bf
Kazuki Takahashi <narita30830613@gmail.com>
2024-05-03 19:45:11 +0900

    bicycle css modified

--- src/components/Bicycle/Bicycle.tsx
+++ src/components/Bicycle/Bicycle.tsx
@@ -2,80 +2,46 @@ import Image from "next/image";
 import styles from "./index.module.css";
 function Bicycle() {
   return (
-    <div>
+    <section className={styles.bicycle_box}>
       <div className={styles.bicycle}>
-        <div id="bicycle">Bicycle</div>
+        <h3 id="bicycle">Bicycle</h3>
       </div>
-      <Image
-        className={styles.border}
-        src="/border_2.svg"
-        alt="icon"
-        width={122}
-        height={0}
-      />
       <div className={styles.bike_items}>
-        <div className={styles.bike_item1}>
+        <div className={styles.bike_item}>
           <Image
-            className={styles.bike1_icon}
+            className={styles.bike_icon}
             src="/bicycle1.png"
             alt="bike1"
             width={274.4}
             height={182}
           />
-          <div className={styles.bike1_title}>タイトルタイトル</div>
-          <div className={styles.bike1_text}>テキストテキストテキスト</div>
+          <h4 className={styles.bike_title}>タイトルタイトル</h4>
+          <div className={styles.bike_text}>テキストテキストテキスト</div>
         </div>
-        <div className={styles.bike_item1}>
+        <div className={styles.bike_item}>
           <Image
-            className={styles.bike1_icon}
+            className={styles.bike_icon}
             src="/bicycle2.png"
             alt="bike2"
             width={274.4}
             height={182}
           />
-          <div className={styles.bike1_title}>タイトルタイトル</div>
-          <div className={styles.bike1_text}>テキストテキストテキスト</div>
+          <h4 className={styles.bike_title}>タイトルタイトル</h4>
+          <div className={styles.bike_text}>テキストテキストテキスト</div>
         </div>
-        <div className={styles.bike_item1}>
+        <div className={styles.bike_item}>
           <Image
-            className={styles.bike1_icon}
+            className={styles.bike_icon}
             src="/bicycle3.png"
             alt="bike3"
             width={274.4}
             height={182}
           />
-          <div className={styles.bike1_title}>タイトルタイトル</div>
-          <div className={styles.bike1_text}>テキストテキストテキスト</div>
+          <h4 className={styles.bike_title}>タイトルタイトル</h4>
+          <div className={styles.bike_text}>テキストテキストテキスト</div>
         </div>
       </div>
-      {/* <div className={styles.div1}>タイトルタイトル</div>
-      <div className={styles.div2}>テキストテキストテキスト</div>
-      <div className={styles.div3}>タイトルタイトル</div>
-      <div className={styles.div4}>テキストテキストテキスト</div>
-      <div className={styles.div5}>タイトルタイトル</div>
-      <div className={styles.div6}>テキストテキストテキスト</div>
-      <Image
-        className={styles.bicycle1Icon}
-        src="/bicycle1.png"
-        alt="bike1"
-        width={274.4}
-        height={182}
-      />
-      <Image
-        className={styles.bicycle2Icon}
-        src="/bicycle2.png"
-        alt="bike2"
-        width={274.4}
-        height={182}
-      />
-      <Image
-        className={styles.bicycle3Icon}
-        src="/bicycle3.png"
-        alt="bike3"
-        width={274.4}
-        height={182}
-      /> */}
-    </div>
+    </section>
   );
 }
 
